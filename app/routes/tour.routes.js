1195cec5d7b9eac35a633430264ace813ad976b7
ludeesha20 <ludeeshananayakkara.20@gmail.com>
2023-10-10 22:38:57 +0530

    Tour route initiate

---
+++ app/routes/tour.routes.js
@@ -0,0 +1,13 @@
+const { hotel } = require("../models/index.js");
+
+module.exports = app => {
+    const trip = require("../controllers/trip.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Store a new destination
+    router.post("/locations", tour.create);
+
+    app.use("/api/tour", router);
+  };
+  
